130df1001b713b98df4ca1e89539dd72cd489798
Vipin Cubet <Vipin@cubettech.com>
2019-01-29 18:05:51 +0530

    Swift 2 files  added

---
+++ swift/GmailDataModel.swift
@@ -0,0 +1,180 @@
+//
+//  GmailDataModel.swift
+//  EmailClient
+//
+//  Created by Vivek iLeaf on 12/27/18.
+//  Copyright © 2018 Vivek iLeaf. All rights reserved.
+//
+
+import Foundation
+//MARK:- All Message Mail List with ids
+
+struct MessageList : Codable {
+  var messages : [Messages]?
+  var nextPageToken: String?
+  let resultSizeEstimate : Int?
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case messages = "messages"
+    case nextPageToken = "nextPageToken"
+    case resultSizeEstimate = "resultSizeEstimate"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    messages = try values.decodeIfPresent([Messages].self, forKey: .messages)
+    nextPageToken = try values.decodeIfPresent(String.self, forKey: .nextPageToken)
+    resultSizeEstimate = try values.decodeIfPresent(Int.self, forKey: .resultSizeEstimate)
+}
+}
+//MARK:- Each Mail Message Model
+  struct Messages : Codable {
+    let id : String?
+    let threadId : String?
+    
+    enum CodingKeys: String, CodingKey {
+      
+      case id = "id"
+      case threadId = "threadId"
+    }
+    
+    init(from decoder: Decoder) throws {
+      let values = try decoder.container(keyedBy: CodingKeys.self)
+      id = try values.decodeIfPresent(String.self, forKey: .id)
+      threadId = try values.decodeIfPresent(String.self, forKey: .threadId)
+}
+}
+
+
+struct MessageData : Codable {
+  let id : String?
+  let threadId : String?
+  let labelIds : [String]?
+  let snippet : String?
+  let historyId : String?
+  let internalDate : String?
+  let payload : Payload?
+  let sizeEstimate : Int?
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case id = "id"
+    case threadId = "threadId"
+    case labelIds = "labelIds"
+    case snippet = "snippet"
+    case historyId = "historyId"
+    case internalDate = "internalDate"
+    case payload = "payload"
+    case sizeEstimate = "sizeEstimate"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    id = try values.decodeIfPresent(String.self, forKey: .id)
+    threadId = try values.decodeIfPresent(String.self, forKey: .threadId)
+    labelIds = try values.decodeIfPresent([String].self, forKey: .labelIds)
+    snippet = try values.decodeIfPresent(String.self, forKey: .snippet)
+    historyId = try values.decodeIfPresent(String.self, forKey: .historyId)
+    internalDate = try values.decodeIfPresent(String.self, forKey: .internalDate)
+    payload = try values.decodeIfPresent(Payload.self, forKey: .payload)
+    sizeEstimate = try values.decodeIfPresent(Int.self, forKey: .sizeEstimate)
+  }
+  
+}
+
+struct Payload : Codable {
+  let partId : String?
+  let mimeType : String?
+  let filename : String?
+  let headers : [Headers]?
+  let body : Body?
+  let parts : [Parts]?
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case partId = "partId"
+    case mimeType = "mimeType"
+    case filename = "filename"
+    case headers = "headers"
+    case body = "body"
+    case parts = "parts"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    partId = try values.decodeIfPresent(String.self, forKey: .partId)
+    mimeType = try values.decodeIfPresent(String.self, forKey: .mimeType)
+    filename = try values.decodeIfPresent(String.self, forKey: .filename)
+    headers = try values.decodeIfPresent([Headers].self, forKey: .headers)
+    body = try values.decodeIfPresent(Body.self, forKey: .body)
+    parts = try values.decodeIfPresent([Parts].self, forKey: .parts)
+  }
+  
+}
+
+struct Headers : Codable {
+  let name : String?
+  let value : String?
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case name = "name"
+    case value = "value"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    name = try values.decodeIfPresent(String.self, forKey: .name)
+    value = try values.decodeIfPresent(String.self, forKey: .value)
+  }
+  
+}
+
+struct Body : Codable {
+  let size : Int?
+  let data : String?
+  let attachmentId:String?
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case size = "size"
+    case data = "data"
+    case attachmentId = "attachmentId"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    size = try values.decodeIfPresent(Int.self, forKey: .size)
+    data = try values.decodeIfPresent(String.self, forKey: .data)
+    attachmentId = try values.decodeIfPresent(String.self, forKey: .attachmentId)
+  }
+  
+}
+
+struct Parts : Codable {
+  let partId : String?
+  let mimeType : String?
+  let filename : String?
+  let headers : [Headers]?
+  let body : Body?
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case partId = "partId"
+    case mimeType = "mimeType"
+    case filename = "filename"
+    case headers = "headers"
+    case body = "body"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    partId = try values.decodeIfPresent(String.self, forKey: .partId)
+    mimeType = try values.decodeIfPresent(String.self, forKey: .mimeType)
+    filename = try values.decodeIfPresent(String.self, forKey: .filename)
+    headers = try values.decodeIfPresent([Headers].self, forKey: .headers)
+    body = try values.decodeIfPresent(Body.self, forKey: .body)
+  }
+  
+}
---
+++ swift/MessageListModel.swift
@@ -0,0 +1,117 @@
+//
+//  MessageListModel.swift
+//  EmailClient
+//
+//  Created by Vivek iLeaf on 12/26/18.
+//  Copyright © 2018 Vivek iLeaf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MailBoxData {
+  var messageMetaData : MessageList?
+  var messageDetails : [MessageData] = []
+  static let shared = MailBoxData()
+}
+//MARK:- Extension for Message Data for handing the gmail Metadata
+extension MessageData
+{
+  //Decode TEXT encrypted body from Gmail API
+  func extractBodyText()->String
+  {
+    let payload: Payload = self.payload!
+    if payload.parts != nil
+    {
+    if ((payload.parts?.filter{$0.mimeType == "text/plain"})?.count)! > 0
+    {
+      return ((payload.parts?.filter{$0.mimeType == "text/plain"}.first!.body?.data)!.decryptMessage() ?? "")
+      }
+      else
+    {
+      return ((payload.parts?.filter{$0.mimeType == "text/html"}.first!.body?.data)!.decryptMessage() ?? "")
+      }
+    
+    }
+    else
+    {
+      return (payload.body?.data?.decryptMessage() ?? "")!
+    }
+
+  }
+  //Whether Mail has attachment
+  func isAttachment()->Bool
+  {
+    if self.payload?.mimeType == "multipart/related" || self.payload?.mimeType == "multipart/mixed" || self.payload?.mimeType == "multipart/report"
+    {
+      return true
+    }
+    else
+    {
+      return false
+    }
+  }
+   //Decode TEXT encrypted body from Gmail API
+  func extractBodyHTML()->String
+  {
+    let payload: Payload = self.payload!
+    if payload.parts != nil
+    {
+      if ((payload.parts?.filter{$0.mimeType == "text/html"})?.count)! > 0
+    {
+      return ((payload.parts?.filter{$0.mimeType == "text/html"}.first!.body?.data)!).decryptMessage() ?? ""
+      }
+      else
+    {
+      return ((payload.parts?.filter{$0.mimeType == "text/plain "}.first!.body?.data)!).decryptMessage() ?? ""
+      }
+    }
+    else
+    {
+      return (payload.body?.data)!.decryptMessage() ?? ""
+    }
+    
+  }
+ //Extract Email Sender Name from metadata
+  func extractSendername()->String
+  {
+    return self.payload?.headers?.filter{$0.name == "From"}.first?.value == nil ? "" : (self.payload?.headers?.filter{$0.name == "From"}.first?.value!)!
+  }
+  //Extract email Snippet Name from metadata
+  func extractSnippet()-> String
+  {
+    return self.snippet == nil ? "" : self.snippet!
+  }
+  //Extract email message time from metadata
+  func extractMessageTime()->String
+  {
+    return self.payload?.headers?.filter{$0.name == "Date"}.first?.value == nil ? "" : (self.payload?.headers?.filter{$0.name == "Date"}.first?.value!)!
+  }
+  //Extract reciever name from metadata
+  func extractrecieversName()->String
+  {
+      return self.payload?.headers?.filter{$0.name == "To"}.first?.value == nil ? "" : (self.payload?.headers?.filter{$0.name == "To"}.first?.value!)!
+  }
+  //Extract subject from metadata
+  func extractSubject()->String
+  {
+    return self.payload?.headers?.filter{$0.name == "Subject"}.first?.value == nil ? "" : (self.payload?.headers?.filter{$0.name == "Subject"}.first?.value!)!
+  }
+  //Extract cc recipents from metadata
+  func extractCCperson()->String
+  {
+    return self.payload?.headers?.filter{$0.name == "Cc"}.first?.value == nil ? "" : (self.payload?.headers?.filter{$0.name == "Cc"}.first?.value!)!
+  }
+  //Extract bcc recipents from metadata
+  func extractBCCPerson()->String
+  {
+     return self.payload?.headers?.filter{$0.name == "Bcc"}.first?.value == nil ? "" : (self.payload?.headers?.filter{$0.name == "Bcc"}.first?.value!)!
+  }
+  //Extract messageTime in string Format
+  func extractMessageTimeFromTimeStamp()->String
+  {
+    let timeInterval : TimeInterval = TimeInterval(self.internalDate!)!
+    let messageDate : Date = Date(timeIntervalSince1970: timeInterval/1000)
+    return messageDate.dateToString()
+  }
+}
+
