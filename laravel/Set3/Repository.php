eb673a356399bcb646cc8d2975c374b03faf3924
Nandul Das <nanduldas@Nanduls-MacBook-Pro.local>
2022-02-03 17:34:25 +0530

    New Laravel files added

--- laravel/Set3/Repository.php
+++ laravel/Set3/Repository.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Repositories;
 
 use Admin\Repositories\UserRepository as AdminUserRepository;
@@ -27,10 +26,57 @@ class NotificationListenerRepository extends BaseRepository
         $this->pushClientScopeCriteria();
     }
 
-    //
+    /**
+     * @param Builder|Model $query
+     * @param Entity $arguments
+     * @return Builder
+     */
+    protected function filter($query, $arguments)
+    {
+        $query = $query instanceof Model ? $query->newQuery() : $query;
+
+        if ($arguments->get("type"))
+        {
+            $query->whereIn("type", array_wrap($arguments->get("type")));
+        }
+
+        return $query;
+    }
+
+    /**
+     * @return User[]|Collection
+     */
+    public function getNotifyTargets()
+    {
+        $notifiableRoles = applicationRoles()->getAllRoles()
+            ->diff(["learner", "author"]);
+
+        $userRepository = new AdminUserRepository($this->getClientScopeCriteria()
+            ->getFor());
+
+        $arguments = new Entity(["type" => "all", "scopeRoles" => $notifiableRoles->toArray() , "roles" => $notifiableRoles->toArray() , "paging" => 100000, "groups" => false, ]);
+
+        return $userRepository->get($arguments);
+    }
+
+    /**
+     * @param Collection $userIds
+     * @return Collection|User[]
+     */
+    protected function getNotifyUsers($userIds)
+    {
+        $clientScopeCriteria = $this->getClientScopeCriteria();
+        return User::query()
+            ->whereIn("client_id", array_wrap($clientScopeCriteria->getFor()))
+            ->whereIn("id", $userIds->keys()
+            ->toArray())
+            ->get()
+            ->keyBy("id");
+    }
     public function listing($arguments)
     {
-        $clientKey = $this->getClientScopeCriteria()->getFor();
+        $clientKey = $this->getClientScopeCriteria()
+            ->getFor();
 
         $clientUserRepository = new ClientUserRepository();
 
@@ -45,86 +91,41 @@ class NotificationListenerRepository extends BaseRepository
         $userIds = collect([]);
         $collection = $list->getCollection();
 
-        $collection->each(function ($item) use ($userIds) {
+        $collection->each(function ($item) use ($userIds)
+        {
             /** @var \App\Models\Notifications\NotificationListener $item */
-            $item->getNotifyUsers()->each(function ($userId) use ($userIds) {
+            $item->getNotifyUsers()->each(function ($userId) use ($userIds)
+            {
                 $userIds->put($userId, $userId);
             });
         });
 
         $this->users = $this->getNotifyUsers($userIds);
 
-        $collection->each(function ($item) {
+        $collection->each(function ($item)
+        {
             $this->mapGroups($item);
 
             /** @var \App\Models\Notifications\NotificationListener $item */
             $item->users = collect();
 
-            $item->getNotifyUsers()->each(function ($userId) use ($item) {
-                $user = $this->users->get($userId);
-                if (!$user) {
+            $item->getNotifyUsers()->each(function ($userId) use ($item)
+            {
+                $user = $this
+                    ->users
+                    ->get($userId);
+                if (!$user)
+                {
                     return;
                 }
 
-                $item->users->put($userId, $user);
+                $item
+                    ->users
+                    ->put($userId, $user);
             });
         });
 
         return $list;
-    }
-
-    /**
-     * @param Builder|Model $query
-     * @param Entity $arguments
-     * @return Builder
-     */
-    protected function filter($query, $arguments)
-    {
-        $query = $query instanceof Model ? $query->newQuery() : $query;
-
-        if ($arguments->get("type")) {
-            $query->whereIn("type", array_wrap($arguments->get("type")));
-        }
-
-        return $query;
-    }
-
-    /**
-     * @return User[]|Collection
-     */
-    public function getNotifyTargets()
-    {
-        $notifiableRoles = applicationRoles()
-            ->getAllRoles()
-            ->diff(["learner", "author"]);
-
-        $userRepository = new AdminUserRepository(
-            $this->getClientScopeCriteria()->getFor()
-        );
-
-        $arguments = new Entity([
-            "type" => "all",
-            "scopeRoles" => $notifiableRoles->toArray(),
-            "roles" => $notifiableRoles->toArray(),
-            "paging" => 100000,
-            "groups" => false,
-        ]);
-
-        return $userRepository->get($arguments);
-    }
-
-    /**
-     * @param Collection $userIds
-     * @return Collection|User[]
-     */
-    protected function getNotifyUsers($userIds)
-    {
-        $clientScopeCriteria = $this->getClientScopeCriteria();
-        return User::query()
-            ->whereIn("client_id", array_wrap($clientScopeCriteria->getFor()))
-            ->whereIn("id", $userIds->keys()->toArray())
-            ->get()
-            ->keyBy("id");
     }
 
     protected function mapGroups($item)
@@ -133,23 +134,27 @@ class NotificationListenerRepository extends BaseRepository
 
         $item->in = collect();
 
-        foreach ($groups->get("divisions", []) as $key) {
-            if ($name = $this->divisions->get($key)) {
-                $item->in->push(
-                    (object) [
-                        "type" => "divisions",
-                        "id" => $key,
-                        "name" => $name,
-                    ]
-                );
+        foreach ($groups->get("divisions", []) as $key)
+        {
+            if ($name = $this
+                ->divisions
+                ->get($key))
+            {
+                $item
+                    ->in
+                    ->push((object)["type" => "divisions", "id" => $key, "name" => $name, ]);
             }
         }
 
-        foreach ($groups->get("teams", []) as $key) {
-            if ($name = $this->teams->get($key)) {
-                $item->in->push(
-                    (object) ["type" => "teams", "id" => $key, "name" => $name]
-                );
+        foreach ($groups->get("teams", []) as $key)
+        {
+            if ($name = $this
+                ->teams
+                ->get($key))
+            {
+                $item
+                    ->in
+                    ->push((object)["type" => "teams", "id" => $key, "name" => $name]);
             }
         }
 
@@ -161,3 +166,4 @@ class NotificationListenerRepository extends BaseRepository
         return NotificationListener::class;
     }
 }
+
